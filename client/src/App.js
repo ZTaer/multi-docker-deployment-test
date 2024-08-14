b4428c692274e48a32209ad750c32de3022be0a0
zt <ztaerkiller@gmail.com>
2024-08-14 15:06:32 +0800

    v0.0.1

--- client/src/App.js
+++ client/src/App.js
@@ -20,7 +20,7 @@ function App() {
           >
             Learn React v0.0.1
           </a>
-          <Link to="/">Home</Link>
+          <Link to="/">Home v0.0.1</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
         <div>
